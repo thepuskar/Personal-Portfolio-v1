951ad7e034b481109fc9fd79b906966158aaef92
devpuskar <puskar.ad@gmail.com>
2020-05-28 16:36:45 +0545

    testimonials slider slide functionality enable

--- js/app.js
+++ js/app.js
@@ -2,52 +2,51 @@ const filterBtn = document.querySelector("#filterBtn").children;
 const item = document.querySelector(".gallery").children;
 
 for (let i = 0; i < filterBtn.length; i++) {
-    filterBtn[i].addEventListener("click", function () {
-        for (let j = 0; j < filterBtn.length; j++) {
-            filterBtn[j].classList.remove("active");
-        }
-        this.classList.add("active");
-        const target = this.getAttribute("data-target")
-        for (let k = 0; k < item.length; k++) {
-            item[k].style.display = "none";
-            if (target == item[k].getAttribute("data-id")) {
-                item[k].style.display = "block";
-            }
-            if (target == "all") {
-                item[k].style.display = "block";
-            }
-        }
-    })
+  filterBtn[i].addEventListener("click", function () {
+    for (let j = 0; j < filterBtn.length; j++) {
+      filterBtn[j].classList.remove("active");
+    }
+    this.classList.add("active");
+    const target = this.getAttribute("data-target");
+    for (let k = 0; k < item.length; k++) {
+      item[k].style.display = "none";
+      if (target == item[k].getAttribute("data-id")) {
+        item[k].style.display = "block";
+      }
+      if (target == "all") {
+        item[k].style.display = "block";
+      }
+    }
+  });
 }
 
 const closeLightbox = document.querySelector(".close-lightbox");
 const lightbox = document.querySelector(".lightbox");
-const lightboxImg = lightbox.querySelector("img")
+const lightboxImg = lightbox.querySelector("img");
 
-lightbox.addEventListener('click', function () {
-    if (event.target != lightboxImg) {
-        lightbox.classList.remove("show")
-        lightbox.classList.add("hide")
-    }
-})
+lightbox.addEventListener("click", function () {
+  if (event.target != lightboxImg) {
+    lightbox.classList.remove("show");
+    lightbox.classList.add("hide");
+  }
+});
 
 closeLightbox.addEventListener("click", function () {
-    lightbox.classList.remove("show")
-    lightbox.classList.add("hide")
-})
+  lightbox.classList.remove("show");
+  lightbox.classList.add("hide");
+});
 
 const gallery = document.querySelector(".gallery");
 
-const galleryItem = document.querySelectorAll('.item')
+const galleryItem = document.querySelectorAll(".item");
 
 galleryItem.forEach(function (element) {
-    element.querySelector(".fa-plus").addEventListener("click", function () {
-        lightbox.classList.remove("hide")
-        lightbox.classList.add("show")
-        lightboxImg.src = element.querySelector("img").getAttribute("src");
-    })
-})
-
+  element.querySelector(".fa-plus").addEventListener("click", function () {
+    lightbox.classList.remove("hide");
+    lightbox.classList.add("show");
+    lightboxImg.src = element.querySelector("img").getAttribute("src");
+  });
+});
 
 const sliderContainer = document.querySelector(".testimonials-box");
 const slider = sliderContainer.children;
@@ -56,49 +55,68 @@ const containerWidth = sliderContainer.offsetWidth;
 const margin = 30;
 let itemPerSlide = 0;
 
-const responsive = [{
-        breakPoint: {
-            width: 0,
-            item: 1
-        }
+const responsive = [
+  {
+    breakPoint: {
+      width: 0,
+      item: 1,
     },
-    {
-        breakPoint: {
-            width: 991,
-            item: 2
-        }
-    }
-]
+  },
+  {
+    breakPoint: {
+      width: 991,
+      item: 2,
+    },
+  },
+];
 
 function load() {
-    for (let i = 0; i < responsive.length; i++) {
-        if (window.innerWidth > responsive[i].breakPoint.width) {
-            itemPerSlide = responsive[i].breakPoint.item;
-        }
+  for (let i = 0; i < responsive.length; i++) {
+    if (window.innerWidth > responsive[i].breakPoint.width) {
+      itemPerSlide = responsive[i].breakPoint.item;
     }
-    start();
+  }
+  start();
 }
 
 function start() {
-    for (let i = 0; i < slider.length; i++) {
-        slider[i].style.width = (containerWidth / itemPerSlide) - margin + "px";
-        slider[i].style.margin = margin / 2 + "px";
-    }
-    sliderContainer.style.width = containerWidth * itemPerSlide + "px";
+  totalWidth = 0;
+  for (let i = 0; i < slider.length; i++) {
+    slider[i].style.width = containerWidth / itemPerSlide - margin + "px";
+    slider[i].style.margin = margin / 2 + "px";
+    totalWidth += containerWidth / itemPerSlide;
+  }
+  sliderContainer.style.width = totalWidth + "px";
 
-    const sliderDot = Math.ceil(slider.length / itemPerSlide);
+  const sliderDot = Math.ceil(slider.length / itemPerSlide);
 
-    for (let i = 0; i < sliderDot; i++) {
-        const div = document.createElement("div");
-        div.id = i;
-        div.setAttribute("onclick", "controlSlide(this)")
-        if (i = 0) {
-            div.classList.add("active")
-        }
-        document.querySelector(".slider").appendChild(div);
+  for (let i = 0; i < sliderDot; i++) {
+    const div = document.createElement("div");
+    div.id = i;
+    div.setAttribute("onclick", "controlSlide(this)");
+    if (i == 0) {
+      div.classList.add("active");
     }
+    document.querySelector(".slider").appendChild(div);
+  }
+}
+
+let currentSlide = 0;
+
+function controlSlide(element) {
+  currentSlide = element.id;
+  changeSlide(currentSlide);
+}
+
+function changeSlide(currentSlide) {
+  controlButton = document.querySelector(".slider").children;
+  for (let i = 0; i < controlButton.length; i++) {
+    controlButton[i].classList.remove("active");
+  }
+  controlButton[currentSlide].classList.add("active");
+
+  sliderContainer.style.marginLeft = -(containerWidth * currentSlide) + "px";
 }
 window.onload = load();
-
 
 // querySelector(".testimonials-img")
