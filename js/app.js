a24765fdd7e19d4e9e3308245921715961f87690
devpuskar <puskar.ad@gmail.com>
2020-05-29 03:35:57 +0545

    navbar fixed while scroll down and link class changed onclick

--- js/app.js
+++ js/app.js
@@ -1,3 +1,33 @@
+//Header fixed
+
+window.onscroll = function () {
+  const docScrollTop = document.documentElement.scrollTop;
+
+  if (window.innerWidth > 991) {
+    if (docScrollTop > 100) {
+      document.querySelector("header").classList.add("fixed");
+    } else {
+      document.querySelector("header").classList.remove("fixed");
+    }
+  }
+}
+
+//navbar links
+
+const navbar = document.querySelector(".navbar");
+a = navbar.querySelectorAll("a")
+
+a.forEach(function (element) {
+  element.addEventListener("click", function () {
+    for (let i = 0; i < a.length; i++){
+      a[i].classList.remove("active");
+    }
+    this.classList.add("active")
+  })
+})
+
+//Portfolio Gallery
+
 const filterBtn = document.querySelector("#filterBtn").children;
 const item = document.querySelector(".gallery").children;
 
